3e074eb0255386276ec44c8d4cec7ee3a7be456a
ThexGameLord <69915399+ThexGameLord@users.noreply.github.com>
2023-09-29 13:08:59 +0100

    Delete delete_existing_build.bat

--- delete_existing_build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Removing previous build of TXCORDAPI
-title TXCORDAPI Build remover
-
-rmdir /Q /S build
-rmdir /Q /S dist
-echo Finished removing previous build of TXCORDAPI
-
-pause
